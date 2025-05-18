3643e77767993639e7a9790319ed168870323640
Luis <luiscvlh11@gmail.com>
2025-05-18 12:36:43 -0300

    chore: :wrench: configura credenciais do google como variaveis de ambiente

--- environment.d.ts
+++ environment.d.ts
@@ -2,6 +2,17 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       GOOGLE_AI_API_KEY: string;
+      GOOGLE_CLOUD_TYPE: string;
+      GOOGLE_CLOUD_PROJECT_ID: string;
+      GOOGLE_CLOUD_PRIVATE_KEY_ID: string;
+      GOOGLE_CLOUD_PRIVATE_KEY: string;
+      GOOGLE_CLOUD_CLIENT_EMAIL: string;
+      GOOGLE_CLOUD_CLIENT_ID: string;
+      GOOGLE_CLOUD_AUTH_URI: string;
+      GOOGLE_CLOUD_TOKEN_URI: string;
+      GOOGLE_CLOUD_AUTH_PROVIDER_X509_CERT_URL: string;
+      GOOGLE_CLOUD_CLIENT_X509_CERT_URL: string;
+      GOOGLE_CLOUD_UNIVERSE_DOMAIN: string;
     }
   }
 }
--- src/app/api/speech-to-text/route.ts
+++ src/app/api/speech-to-text/route.ts
@@ -1,13 +1,21 @@
-import fs from "fs";
-import path from "path";
-
 import speech from "@google-cloud/speech";
 
 import { NextRequest } from "next/server";
 
-const keyFilePath = path.resolve("credentials.json");
-const credentials = JSON.parse(fs.readFileSync(keyFilePath, "utf8"));
-
+const credentials = {
+  type: process.env.GOOGLE_CLOUD_TYPE,
+  project_id: process.env.GOOGLE_CLOUD_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_CLOUD_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_CLOUD_PRIVATE_KEY,
+  client_email: process.env.GOOGLE_CLOUD_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_CLOUD_CLIENT_ID,
+  auth_uri: process.env.GOOGLE_CLOUD_AUTH_URI,
+  token_uri: process.env.GOOGLE_CLOUD_TOKEN_URI,
+  auth_provider_x509_cert_url:
+    process.env.GOOGLE_CLOUD_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.GOOGLE_CLOUD_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.GOOGLE_CLOUD_UNIVERSE_DOMAIN,
+};
 const client = new speech.SpeechClient({ credentials });
 
 export async function POST(req: NextRequest) {
